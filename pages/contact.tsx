c27e6ff8bfef9564091709963ff0ea3bf0c74c93
Godson Oladipupo <godsonoladipupo6@gmail.com>
2022-10-12 01:21:16 -0600

    Vercel fixes2

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,20 +7,13 @@ import PageHeader from "../components/PageHeader/PageHeader";
 const contact = () => {
   return (
     <div className="container">
-      <PageHeader
-        title="Contact"
-        subTitle1="Locations"
-        subTitle2="Contact"
-        // image="nigeria.jpg"
-      />
+      <PageHeader title="Contact" subTitle1="Locations" subTitle2="Contact" />
       <div className="mt-16 md:mt-[100px]">
         <JoinUs title="Contact Us" />
       </div>
       <div className=" flex flex-col items-center mt-16 md:mt-[100px] ">
-        {/* <h1 className="text-[32px] font-bold text-center">Contact Us</h1> */}
         <ContactShort />
       </div>
-      //{" "}
     </div>
   );
 };
