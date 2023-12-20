a693d8c9118c928c822284fca15569f71ed3f264
Godson Oladipupo <godsonoladipupo6@gmail.com>
2023-12-20 12:30:13 -0700

    Added book

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -11,7 +11,7 @@ const Button = forwardRef(({ filled, outline, title, href }: Props) => {
   return (
     <a
       href={href}
-      className={`${filled && "bg-secondary text-white"} ${
+      className={`${filled && "bg-[#FFD55A] text-primary"} ${
         outline &&
         "border border-solid border-secondary bg-transparent text-secondary"
       } text-sm px-[18px] py-3 font-bold w-fit rounded-xl hover:scale-105 transition-transform duration-200 ease-in-out cursor-pointer`}
