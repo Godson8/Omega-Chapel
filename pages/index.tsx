73189b2d469c313b4e10a7add608a38722d480b8
Godson Oladipupo <godsonoladipupo6@gmail.com>
2023-12-20 12:30:13 -0700

    Added book

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,12 @@ import Locations from "../components/Locations/Locations";
 import NavBar from "../components/NavBar/NavBar";
 import Newsletter from "../components/Newsletter/Newsletter";
 import styles from "../styles/Home.module.css";
+import Box from "@mui/material/Box";
+// import Button from '@mui/material/Button';
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import { useEffect, useState } from "react";
+import { IoIosCloseCircle } from "react-icons/io";
 
 const endPoint = `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=${process.env.PLAYLIST_ID}&key=${process.env.YOUTUBE_KEY}`;
 
@@ -21,6 +27,21 @@ const Home = ({ data }: any) => {
   const latestSermonImage = data.items[0].snippet.thumbnails.maxres.url;
   const latestSermonVideoId = data.items[0].snippet.resourceId.videoId;
   const latestSermonTitle = data.items[0].snippet.title;
+
+  const [openModal, setOpenModal] = useState(false);
+
+  useEffect(() => {
+    // Open the modal on component mount
+    setOpenModal(true);
+
+    // Cleanup function to close the modal on component unmount
+    return () => setOpenModal(false);
+  }, []); // Empty dependency array ensures this effect runs once on mount
+
+  const handleModalClose = () => {
+    setOpenModal(false);
+  };
+
   return (
     <div className={`${styles.container}`}>
       <Head>
@@ -71,6 +92,53 @@ const Home = ({ data }: any) => {
       <Generations />
       <Newsletter />
       <FollowUs />
+
+      <Modal
+        open={openModal}
+        onClose={handleModalClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            ...style,
+            width: { xs: 300, sm: 400, md: 600, lg: 800, xl: 1000 }, // Adjust the widths as needed
+            mx: "auto", // Center the modal horizontally
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          <button
+            onClick={handleModalClose}
+            style={{ alignSelf: "flex-end", marginBottom: "10px" }}
+          >
+            <IoIosCloseCircle size={25} color="red" />
+          </button>
+          <h1 className="text-2xl md:text-[32px] font-black text-primary">
+            OUT NOW!
+          </h1>
+          <Typography sx={{ mt: 2 }}>
+            <div className="relative xsOpp:h-[550px] lgOpp:h-[250px] rounded-xl mb-2">
+              <Image
+                src="/components.jpg"
+                layout="fill"
+                alt="7 Components of Faith"
+                className="object-cover"
+              />
+            </div>
+            To purchase the book, send a WhatsApp text to:
+          </Typography>
+          <h1 className="font-black text-xl text-primary font-primary hover:text-secondary">
+            <a
+              href="https://wa.me/2348026842508?text=I%20would%20like%20to%20purchase%20the%20book."
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <strong>+234 802 684 2508</strong>
+            </a>
+          </h1>
+        </Box>
+      </Modal>
     </div>
   );
 };
@@ -86,3 +154,51 @@ export const getServerSideProps = async () => {
     },
   };
 };
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
+// export function BasicModal() {
+//   const [open, setOpen] = useState(false);
+//   const handleOpen = () => setOpen(true);
+//   const handleClose = () => setOpen(false);
+
+//   useEffect(() => {
+//     // Open the modal on component mount
+//     handleOpen();
+
+//     // Cleanup function to close the modal on component unmount
+//     return () => handleClose();
+//   }, []); // Empty dependency array ensures this effect runs once on mount
+
+//   return (
+//     <div>
+//       {/* Remove the button as the modal will be opened on page load */}
+//       {/* <Button onClick={handleOpen}>Open modal</Button> */}
+//       <Modal
+//         open={open}
+//         onClose={handleClose}
+//         aria-labelledby="modal-modal-title"
+//         aria-describedby="modal-modal-description"
+//       >
+//         <Box sx={style}>
+//           <Typography id="modal-modal-title" variant="h6" component="h2">
+//             Text in a modal
+//           </Typography>
+//           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+//             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+//           </Typography>
+//         </Box>
+//       </Modal>
+//     </div>
+//   );
+// }
