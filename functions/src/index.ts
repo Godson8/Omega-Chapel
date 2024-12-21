6bc09aca2ff26b8d37b8549ed9c51a6e857d0f82
Godson Oladipupo <godsonoladipupo6@gmail.com>
2024-12-21 09:39:43 -0700

    Install firebase-functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions/v2";
+import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 import * as nodemailer from "nodemailer";
 import { google } from "googleapis";
