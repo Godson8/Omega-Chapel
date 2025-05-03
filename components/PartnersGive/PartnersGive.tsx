e58d8f5fb1075b0ad6c5f647f93692ebbc686a8b
Godson Oladipupo <godsonoladipupo6@gmail.com>
2025-05-02 20:59:21 -0600

    Added naira account

--- components/PartnersGive/PartnersGive.tsx
+++ components/PartnersGive/PartnersGive.tsx
@@ -102,7 +102,7 @@ const PartnersGive = () => {
               >
                 <Tab
                   icon={<FaCreditCard size={24} />} // Increase icon size
-                  label="Pay with Naira"
+                  label="Pay with Card"
                   value="1"
                   className="flex items-center space-x-2" // Align text and icon
                 />
@@ -240,6 +240,32 @@ const PartnersGive = () => {
                       id="panel1-header"
                     >
                       <Typography className="font-bold">
+                        Naira Account (NGN)
+                      </Typography>
+                    </AccordionSummary>
+                    <AccordionDetails>
+                      <Typography className="space-y-2 text-primary">
+                        <p>
+                          <span className="font-bold">Bank: </span>GTBank
+                        </p>
+                        <p>
+                          <span className="font-bold">Account Number: </span>
+                          0942033954
+                        </p>
+                      </Typography>
+                    </AccordionDetails>
+                  </Accordion>
+
+                  <Accordion
+                    expanded={expanded === "panel2"}
+                    onChange={handleAccordionChange("panel2")}
+                  >
+                    <AccordionSummary
+                      expandIcon={<ExpandMoreIcon />}
+                      aria-controls="panel2-content"
+                      id="panel2-header"
+                    >
+                      <Typography className="font-bold">
                         Dollar Account (USD)
                       </Typography>
                     </AccordionSummary>
@@ -265,13 +291,13 @@ const PartnersGive = () => {
                   </Accordion>
 
                   <Accordion
-                    expanded={expanded === "panel2"}
-                    onChange={handleAccordionChange("panel2")}
+                    expanded={expanded === "panel3"}
+                    onChange={handleAccordionChange("panel3")}
                   >
                     <AccordionSummary
                       expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel2-content"
-                      id="panel2-header"
+                      aria-controls="panel3-content"
+                      id="panel3-header"
                     >
                       <Typography className="font-bold">
                         Pound Account (GBP)
@@ -299,13 +325,13 @@ const PartnersGive = () => {
                   </Accordion>
 
                   <Accordion
-                    expanded={expanded === "panel3"}
-                    onChange={handleAccordionChange("panel3")}
+                    expanded={expanded === "panel4"}
+                    onChange={handleAccordionChange("panel4")}
                   >
                     <AccordionSummary
                       expandIcon={<ExpandMoreIcon />}
-                      aria-controls="panel3-content"
-                      id="panel3-header"
+                      aria-controls="panel4-content"
+                      id="panel4-header"
                     >
                       <Typography className="font-bold">
                         Euro Account (EUR)
